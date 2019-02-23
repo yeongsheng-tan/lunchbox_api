c133910d9fc41ffd2152a905408460d50259bdb9
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2019-02-23 22:34:30 +0800

    add router for web view to / using FoodController#index

--- lib/lunchbox_api_web/router.ex
+++ lib/lunchbox_api_web/router.ex
@@ -1,8 +1,21 @@
 defmodule LunchboxApiWeb.Router do
   use LunchboxApiWeb, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/", LunchboxApiWeb do
+    pipe_through :browser
+    get "/", FoodController, :index
   end
 
   scope "/api/v1", LunchboxApiWeb do
