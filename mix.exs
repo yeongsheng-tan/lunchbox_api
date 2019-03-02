d3737705763c3b7e7d9bc1abc0a76f4431ec15d4
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2019-03-02 15:02:58 +0800

    record working config for cockroachdb & distillery2.0 release to gigalixr

--- mix.exs
+++ mix.exs
@@ -35,9 +35,9 @@ defmodule LunchboxApi.MixProject do
     [
       {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
+      {:phoenix_ecto, "~> 3.6"},
+      {:ecto, "~> 2.2"},
+      {:postgrex, "~> 0.13", hex: :postgrex_cdb, override: true},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
