e0e89f46e57e30c67bad996c2e1e811331f44461
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2019-03-31 09:34:15 +0800

    allow user and food deletion and update

--- lib/lunchbox_api_web/router.ex
+++ lib/lunchbox_api_web/router.ex
@@ -36,6 +36,11 @@ defmodule LunchboxApiWeb.Router do
 
     get "/me", UserController, :show_current_user
     resources "/users", UserController, except: [:new, :edit]
+    delete "/users", UserController, :delete
+    put "/users", UserController, :update
+    
     resources "/foods", FoodController, except: [:new, :edit]
+    delete "/foods", FoodController, :delete
+    put "/foods", FoodController, :update
   end
 end
