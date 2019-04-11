fc0602ace6d1964957827ae0d54109c435de847b
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2019-04-11 23:37:19 +0800

    make cypress tests more robust

--- cypress/integration/food_list.spec.js
+++ cypress/integration/food_list.spec.js
@@ -4,35 +4,43 @@ describe('foods', () => {
       const validUser = {
         "email": "cypress_user@cypress.com",
         "password": "cyPR355.io"
-      }
+      };
 
     // sign-in
     cy.request('POST', '/sign_in', validUser).then((resp) => {
-      expect(resp.body).to.have.property('jwt')
-      jwtToken = resp.body.jwt
-    })
-  })
-  
+        expect(resp.body).to.have.property('jwt');
+        jwtToken = resp.body.jwt;
+    });
+  });
+
   it('returns list of foods', () => {
+      // create 2 food items
+      cy.request({method: 'POST',
+                  url: 'foods',
+                  body: {
+                    "food": {
+                      "name": "coffee",
+                      "status": "roasted"
+                    }
+                  },
+                  headers: {authorization: `Bearer ${jwtToken}`}
+                 });
+      cy.request({method: 'POST',
+                  url: 'foods',
+                  body: {
+                    "food": {
+                      "name": "blue cheese",
+                      "status": "well-aged"
+                    }
+                  },
+                  headers: {authorization: `Bearer ${jwtToken}`}
+                 });
     // get all foods
     cy.request({method: 'GET', url: '/foods',
                 headers: {authorization: `Bearer ${jwtToken}`}
               })
-      .its('body.data')
-      .should('not.be.empty')
-  })
-  
-  it('returns specific food item', () => {
-    const food = {
-      "id": 441841402296270849,
-      "name": "coffee",
-      "status": "roasted"
-    }
-    // get specific food item
-    cy.request({method: 'GET', url: '/foods/441841402296270849',
-                headers: {authorization: `Bearer ${jwtToken}`}
-              })
-      .its('body.data')
-      .should('deep.eq', food)
-  })
+        .its('body.data')
+        .should('not.be.empty')
+        .should('have.lengthOf', 2);
+  });
 })
--- cypress/integration/sign_in.spec.js
+++ cypress/integration/sign_in.spec.js
@@ -1,14 +1,42 @@
 describe('users signin', () => {
+  let jwtToken
+  const signupUser = {
+    "user": {
+      "email": "cypress_user@cypress.com",
+      "password": "cyPR355.io",
+      "password_confirmation": "cyPR355.io"
+    }
+  };
+
   const validUser = {
     "email": "cypress_user@cypress.com",
     "password": "cyPR355.io"
-  }
+  };
+
+  before(function() {
+    // sign-up
+    cy.request('POST', '/sign_up', signupUser).then((resp) => {
+        expect(resp.body).to.have.property('jwt');
+    });
+  });
 
   it('returns user json for valid credentials', () => {
     cy.request('POST', '/sign_in', validUser).then((resp) => {
-      expect(resp.body).to.have.property('jwt')
-      expect(resp.body.jwt).to.be.a('string')
-      expect(resp.body.jwt).to.not.be.empty
-    })
+      expect(resp.body).to.have.property('jwt');
+      expect(resp.body.jwt).to.be.a('string');
+      expect(resp.body.jwt).to.not.be.empty;
+      jwtToken = resp.body.jwt;
+    });
+  });
+
+  it('returns email of the current logged-in user', () => {
+    cy.request({method: 'GET', url: '/me',
+                headers: {authorization: `Bearer ${jwtToken}`}
+              }).then((resp) => {
+                expect(resp.body).to.have.property('id');
+                expect(resp.body).to.have.property('email');
+                expect(resp.body.email).to.equal(validUser.email);
+
+              });
   })
-})
+});
