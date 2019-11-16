f1cf6cc1e15fe739f9f37790cfcc00def9c9b737
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2019-11-16 15:51:02 +0800

    upgrade hex packages

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -33,16 +33,16 @@ defmodule LunchboxApi.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.9"},
+      {:phoenix, "~> 1.4.11"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:basic_auth, "~> 2.2.4"},
-      {:distillery, "~> 2.1.1", runtime: false}
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.2"},
+      {:postgrex, "~> 0.15"},
+      {:gettext, "~> 0.17"},
+      {:jason, "~> 1.1"},
+      {:plug_cowboy, "~> 2.1"},
+      {:basic_auth, "~> 2.2"},
+      {:distillery, "~> 2.1", runtime: false}
     ]
   end
 
