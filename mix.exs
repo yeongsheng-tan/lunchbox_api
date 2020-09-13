887b47fe96be173e5ad6592d9aa788b16ce75ba1
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2020-09-13 16:25:54 +0800

    upgrade to erlang 23.0.4 and yarn packages upgrade

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.3.0",
+      version: "1.3.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
