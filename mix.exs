0f3a30b1458d1b1304d9084c11a8f9ca5c2ff439
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-09-03 09:22:16 +0800

    upgrade

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.6.5",
+      version: "1.6.6",
       elixir: "~> 1.12.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
