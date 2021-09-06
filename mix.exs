c0682e85340638c04961d2490a72148c283a4cf3
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-09-06 13:50:30 +0800

    yarn berry

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.6.8",
+      version: "1.6.9",
       elixir: "~> 1.12.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
