77ddafade6b2331edca20b7ccc5ecd8a111e0261
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2021-12-14 18:02:56 +0800

    Mix.Config to Config

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.7.4",
+      version: "1.7.5",
       elixir: "~> 1.13.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
