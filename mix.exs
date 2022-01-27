f3e825012be51c5aa11097e8483f2e731c18be44
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2022-01-27 17:46:26 +0800

    erlang OTP@24.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.7.8",
+      version: "1.7.9",
       elixir: "~> 1.13.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
