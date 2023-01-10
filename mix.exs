7fb9bfe9c8b33bf2e3055821e7586f3f25e80f31
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2023-01-10 18:54:09 +0800

    phoenix 1.6 + nodejs 19.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.8.12",
+      version: "1.8.13",
       elixir: "~> 1.14.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
