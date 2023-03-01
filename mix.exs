955d1dcfa441bcfdf74426c952d7dd254c0ec74b
Tan Yeong Sheng <yeongsheng@odd-e.com>
2023-03-01 14:06:56 +0800

    hex packages upgrade. Upgrade erlang to 24.3.4.9 and nodejs to 19.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.8.14",
+      version: "1.8.15",
       elixir: "~> 1.14.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
