0aeb51235d81fc5b910ba10e56943a2eb7635798
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2023-10-23 19:55:14 +0800

    minor version upgrade

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.9.1",
+      version: "1.9.2",
       elixir: "~> 1.15.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
