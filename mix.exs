cb06848d9901c35e6c1d51bac7eaadb3cd75a12e
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2023-11-26 15:30:32 +0800

    upgrade to yarn@1.22.21

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.10.1",
+      version: "1.10.2",
       elixir: "~> 1.15.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
