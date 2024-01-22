7cfe3c70e71ae5710d2a9e55804542817afb0d9c
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-01-22 21:51:15 +0800

    ecto Postgrex ssl: true plus OTP 25 & above ssl_opts needed

--- config/runtime.exs
+++ config/runtime.exs
@@ -13,6 +13,16 @@ if config_env() == :prod do
     adapter: Ecto.Adapters.Postgres,
     url: System.get_env("DATABASE_URL"),
     database: "",
+    ssl: true,
+    ssl_opts: [verify: :verify_peer,
+               cacerts: :public_key.cacerts_get(), # supported with OTP26
+               versions: [:"tlsv1.3"],
+               depth: 3,
+               server_name_indication: String.to_charlist(System.get_env("DATABASE_HOST")),
+               customize_hostname_check: [
+                match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+               ]
+              ],
     # Free tier db only allows 1 conn
     pool_size: 2
 
