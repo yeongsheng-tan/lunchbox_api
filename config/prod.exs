3d75bbde6fd0a24893a530189dcf4efd4ad0c814
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-01-22 21:57:56 +0800

    ssl_opts: [ verify: :verify_none ]

--- config/prod.exs
+++ config/prod.exs
@@ -56,15 +56,7 @@ config :lunchbox_api, LunchboxApi.Repo,
   url: System.get_env("DATABASE_URL"),
   database: "",
   ssl: true,
-  ssl_opts: [verify: :verify_peer,
-             cacerts: :public_key.cacerts_get(), # supported with OTP26
-             versions: [:"tlsv1.3"],
-             depth: 3,
-             server_name_indication: String.to_charlist(System.get_env("DATABASE_HOST")),
-             customize_hostname_check: [
-               match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-             ]
-            ],
+  ssl_opts: [verify: :verify_none],
   # Free tier db only allows 1 conn
   pool_size: 2
 
--- config/runtime.exs
+++ config/runtime.exs
@@ -14,15 +14,7 @@ if config_env() == :prod do
     url: System.get_env("DATABASE_URL"),
     database: "",
     ssl: true,
-    ssl_opts: [verify: :verify_peer,
-               cacerts: :public_key.cacerts_get(), # supported with OTP26
-               versions: [:"tlsv1.3"],
-               depth: 3,
-               server_name_indication: String.to_charlist(System.get_env("DATABASE_HOST")),
-               customize_hostname_check: [
-                match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-               ]
-              ],
+    ssl_opts: [verify: :verify_none],
     # Free tier db only allows 1 conn
     pool_size: 2
 
