1e46784b9c5fa0a874ea3bbf1cc21341030e217f
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-05-20 11:31:58 +0800

    upgrade hex packages

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.13.0",
+      version: "1.13.1",
       elixir: "~> 1.16.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
