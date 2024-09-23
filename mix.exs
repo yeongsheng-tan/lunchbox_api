a17a1786f5a38d6ce22a1f59a927f59b37620adf
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-09-23 15:49:57 +0800

    elixir@1.17.3 erlang@27.1.0

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.15.6",
-      elixir: "~> 1.17.2",
+      version: "1.15.7",
+      elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
