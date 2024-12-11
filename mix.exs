01311db2569c8612b4aaab96e436c73bd4d74905
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2024-12-11 16:05:51 +0800

    update hex deps and phoenix_live_view node dep

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.15.11",
+      version: "1.15.12",
       elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
