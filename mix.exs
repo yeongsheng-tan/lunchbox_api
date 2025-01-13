82b1b246ce1ab612ce2f0fb267e996b38b48f795
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2025-01-13 10:43:21 +0800

    upgrade elixir@1.18.1 erlang@27.2 nodejs@22.11.0 & hex packages

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.15.12",
-      elixir: "~> 1.17.3",
+      version: "1.16.0",
+      elixir: "~> 1.18.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
