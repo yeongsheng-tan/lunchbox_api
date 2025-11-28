d28444bb46f120368570d379d8a026963d113bdd
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2025-11-28 11:32:09 +0800

    fix js-yaml has prototype pollution in merge

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.17.12",
+      version: "1.17.13",
       elixir: "~> 1.18.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
