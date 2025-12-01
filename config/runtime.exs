7401b894e15fe2f9eeba464ee0b65bdf3d6e4b4d
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2025-12-01 16:32:19 +0800

    removed live_reload config from runtime.exs due to regex serialization error & is only a dev-only feature; removed deprecated config/releases.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,14 +17,4 @@ if config_env() == :prod do
     ssl_opts: [verify: :verify_none],
     # Free tier db only allows 1 conn
     pool_size: 2
-
-  config :lunchbox_api, LunchboxApiWeb.Endpoint,
-    live_reload: [
-      patterns: [
-        ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
-        ~r"priv/gettext/.*(po)$",
-        ~r"lib/lunchbox_api_web/(live|views)/.*(ex)$",
-        ~r"lib/lunchbox_api_web/templates/.*(eex)$"
-      ]
-    ]
 end
