e3a64f7af9d8e105328829987f2331728a110c12
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2025-12-30 14:27:24 +0800

    postgresql@17.7 & upgrade local hex packages

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.18.5",
+      version: "1.18.6",
       elixir: "~> 1.19.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
