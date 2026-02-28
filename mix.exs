945fde4376c0786cea988a4178751b7f78a240de
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2026-02-28 23:30:06 +0800

    pnpm@10.30.3 phoenix@1.8.4 phoenix_live_view@1.1.25 topbar@3.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LunchboxApi.MixProject do
   def project do
     [
       app: :lunchbox_api,
-      version: "1.18.14",
+      version: "1.18.15",
       elixir: "~> 1.19.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
